f933925d510b8b245dacd11bd978c3394ca455f0
c0a242182b <c0a242182b@edu.teu.ac.jp>
2025-04-22 17:15:09 +0900

    演習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
-
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -29,7 +29,26 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
          tate = False
      return yoko, tate
 
+def gameover(screen: pg.Surface) -> None:
+    black = pg.Surface((WIDTH,HEIGHT))
+    pg.draw.rect(black,(0,0,0),(0,0,1300,1300),)
+    black.set_alpha(100)
+    screen.blit(black,(0,0))
+
+    font=pg.font.Font(None,80)
+    txt = font.render("Game Over" ,True,(255,255,255))
+    screen.blit(txt,[400,300])
+
+    img = pg.image.load("fig/8.png")
+    screen.blit(img,[340,290])
+    screen.blit(img,[720,290])
+    
+
+
+
 def main():
+    tmr=0
+    clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
@@ -44,12 +63,6 @@ def main():
     bb_rct.centery = random.randint(0,HEIGHT)
     bb_img.set_colorkey((0, 0, 0))
     vx, vy = +5,+5
-        
-
-    
-    
-    clock = pg.time.Clock()
-    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -58,9 +71,13 @@ def main():
 
         # こうかとんRectと爆弾Rectが重なっていたら
         if kk_rct.colliderect(bb_rct): 
-             print("Game Over")
-             return
-
+            gameover(screen)
+            
+            pg.display.update()
+            time.sleep(5)
+            
+            
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key,mv in DELTA.items():
@@ -84,6 +101,19 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+    
+
+
+    
+    
+    clock = pg.time.Clock()
+    tmr = 0
+
+
+
+    
+
+        
 
 
 if __name__ == "__main__":
